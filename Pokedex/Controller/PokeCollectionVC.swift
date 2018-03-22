c41c72f2139666a9bbb9e5a729a8bb2a57dc0d71
Lukáš Růžička <lukas.ruzicka@synetech.cz>
2018-03-22 11:18:24 +0100

    Loading CSV data

--- Pokedex/Controller/PokeCollectionVC.swift
+++ Pokedex/Controller/PokeCollectionVC.swift
@@ -12,18 +12,40 @@ class PokeCollectionVC: UIViewController, UICollectionViewDelegate, UICollection
     
     @IBOutlet weak var pokeCollection: UICollectionView!
     
+    var pokemons = [Pokemon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         pokeCollection.dataSource = self
         pokeCollection.delegate = self
+        
+        parsePokemonsCSV()
+    }
+    
+    func parsePokemonsCSV() {
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+        
+        do {
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            
+            for row in rows {
+                let id = Int(row["id"]!)!
+                let name = row["identifier"]!
+                
+                pokemons.append(Pokemon(name: name, id: id))
+            }
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
     }
 
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
             
-            let pokemon = Pokemon(name: "Pokemon", id: indexPath.row + 1)
-            cell.configureCell(pokemon: pokemon)
+            cell.configureCell(pokemon: pokemons[indexPath.row])
             
             return cell
         }
@@ -35,7 +57,7 @@ class PokeCollectionVC: UIViewController, UICollectionViewDelegate, UICollection
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 30
+        return pokemons.count
     }
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
